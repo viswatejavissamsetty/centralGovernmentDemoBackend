48d2132776c71cebf6abd221d08168b053c24bc0
viswateja994 <viswateja994@gmail.com>
2022-11-17 20:29:13 +0530

    added registration blocker

--- routes/user.py
+++ routes/user.py
@@ -7,6 +7,7 @@ from bson import ObjectId
 
 user = APIRouter()
 
+
 @user.get("/users")
 async def find_all_users():
     return usersEntity(userDb.find())
@@ -20,14 +21,23 @@ async def find_one_user(id):
 @user.post("/user")
 async def register_user(user: UserPost):
     if (user.password != user.confirmPassword):
-        raise HTTPException(status.HTTP_400_BAD_REQUEST, "Password didnot matched")
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST,
+            "Password didnot matched"
+        )
+    if userDb.find_one({"email": user.email}):
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST,
+            "Existing email id unable to register"
+        )
     newUser = User(**dict(user))
     a = userDb.insert_one(dict(newUser))
     created_user = userDb.find_one({"_id": a.inserted_id})
     return userEntity(created_user)
 
+
 @user.post("/user/login")
-async def login_user(login:LoginUser):
+async def login_user(login: LoginUser):
     user = userDb.find_one(
         {
             "email": login.email,
@@ -35,8 +45,10 @@ async def login_user(login:LoginUser):
         }
     )
     if not user:
-        raise HTTPException(status.HTTP_401_UNAUTHORIZED, "Invalid User Id or Password")
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                            "Invalid User Id or Password")
     return userEntity(user)["_id"]
+
 
 @user.patch("/user/update-profile/{id}")
 async def update_user_profile(id, user: UpdateUser):
