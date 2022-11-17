63340ddc320c97661340a0b97096f34baf02482d
viswateja994 <viswateja994@gmail.com>
2022-11-17 19:38:05 +0530

    added heroku commands

--- index.py
+++ index.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.user import user
 
 app = FastAPI(title="Test project")
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(user)
--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Required
 
 
 class User(BaseModel):
